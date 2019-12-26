fbc54d40931463087a93f7d7ddb07751a307afc4
Cornelius <35144948+TheCornelius@users.noreply.github.com>
2019-12-26 13:15:55 +0800

    Modified README.md, inserted images and sample files, modified file names in SQL_InsertValue.py

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
-# SQL-Insert-Statement-Converter
+# SQL Insert Statement Converter (SQL)
 Python script that converts records in tables (eg: .xlsx tables) to SQL's INSERT statements.
 
-# Introduction
+## Introduction
 In a Database class, the lecturer required us to convert records from 4 tables (.xlsx tables) to SQL INSERT statements. Instead of inserting the values manually and formatting it properly, I wrote a Python script that does the job.
 
-Here are the 4 tables
+_Here are the 4 tables:_
+1. Item Table
+![Item Table](/Table_Images/ItemsTable.png)
+
+2. Sales Table<br />
+![Sales Table](/Table_Images/SalesTable.png)
+3. Buyer Table
+![Buyer Table](./Table_Images/BuyerTable.png)
+4. Invoice Table
+![Invoice Table](./Table_Images/InvoiceTable.png)
+
+## What I Did
+1) Copied the contents of the table and saved it as .txt files (File Names: Items.txt, Sales.txt, Buyer.txt, Invoice.txt). <br />
+2) Used Python to read through the .txt files and produce the INSERT statements with appropriate formatting. The program will then output the INSERT statements respective of each table into new .txt files (File Names: ItemsTable.txt, SalesTable.txt, BuyerTable.txt, InvoiceTable.txt).
+
+## User Manual
+1) Download this repository as a zip file (or clone it). <br />
+2) Run "SQL_InsertValue.py"
---
+++ SQL_InsertValue.py
@@ -0,0 +1,46 @@
+# This program was written during Lab 6 of Database Fundamentals - to convert the records of different tables inside the tutorial document to SQL INSERT commands
+# Variables in this program are in the global scope, which will be accessed from the different functions that are defined
+
+# These lists uses these indexes to keep track of different table records: {0-Invoice 1-Buyer 2-Sales 3-Items}
+raw_strings = [r"INSERT INTO Invoice VALUES ({}, '{}', {}, {}, {}, {});", r"INSERT INTO Buyer VALUES ({}, '{}', '{}', '{}');", r"INSERT INTO Sales VALUES ({}, '{}', '{}');", r"INSERT INTO Sales VALUES ({}, '{}', '{}', {}, {}, '{}');"]
+inputFileNames = ["Invoice.txt", "Buyer.txt", "Sales.txt", "Items.txt"]
+outputFileNames = ["InvoiceTable.txt", "BuyerTable.txt", "SalesTable.txt", "ItemsTable.txt"]
+outputCommands = [r"lines[0], lines[1], lines[2], lines[3], lines[4], lines[5]", r"lines[0], lines[1], lines[2], lines[3]", r"lines[0], lines[1], lines[2]", r"lines[0], lines[1], lines[2], lines[3], lines[4], lines[5]"]
+strings = []
+lines = []
+fileLines = []
+command = ""  # String used with exec() to convert string to Python code
+
+def readFromFile(i):
+    """This function is used to read table records from a .txt file, the filenames are in the 'inputFileNames' list.
+        This function takes a parameter - index of 'inputFileNames'.
+    """
+    with open(inputFileNames[i], 'r') as f:
+        fileLines = f.readlines()
+        for element in fileLines:
+            lines = element.replace('\n', '').split('\t')
+            command = "strings.append(raw_strings[i].format(" + outputCommands[i] + "))"
+            exec(command)
+
+def outputToFile(i):
+    """This function is used to write SQL codes to a .txt file, the filenames are in the 'outputFileNames' list
+        This function takes a parameter - index of 'outputFileNames'.
+    """
+    with open(outputFileNames[i], 'w') as f:
+        for string in strings:
+            f.write(string + '\n')
+
+def clearLists():
+    """This function is used to clear the lists used for operation in every iteration of the for loop (to prevent existing data from interrupting future data)"""
+    strings.clear()
+    lines.clear()
+    fileLines.clear()
+
+# Main Function Used For Formatting {Invoice, Buyer, Sales, Items} Tables
+def main():
+    for i in list(range(0, 4)):
+        readFromFile(i)
+        outputToFile(i)
+        clearLists()
+
+main()
